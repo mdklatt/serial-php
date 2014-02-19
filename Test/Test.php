ce4d669b625e11c7b3da1f16d0cc23f9256da394
Michael Klatt <mdklatt@ou.edu>
2014-02-19 13:03:46 -0600

    code clean-up in Test/Test.php

--- Test/Test.php
+++ Test/Test.php
@@ -11,12 +11,10 @@ class Test
      */
     public static function run($name=null)
     {
-        echo 'Test: '.$name.PHP_EOL;
         $path = dirname(__FILE__).DIRECTORY_SEPARATOR;
         if ($name) {
             $path.= "{$name}.php";
         }
-        // system('phpunit --configuration Test/phpunit.xml '.$path, $status);
         system('phpunit --bootstrap Test/env.php '.$path, $status);
         return $status;
     }
