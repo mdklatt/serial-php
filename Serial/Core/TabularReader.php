b97cf5745efe06d803792c8746c1a58355180603
Michael Klatt <mdklatt@ou.edu>
2014-02-27 17:03:27 -0600

    added TabularReader::open() method

--- Serial/Core/TabularReader.php
+++ Serial/Core/TabularReader.php
@@ -8,8 +8,39 @@
  */
 abstract class Serial_Core_TabularReader extends Serial_Core_Reader
 {
+    protected $closing = false;
     protected $stream;
     protected $fields;
+
+    /**
+     * Create a reader with automatic stream-handling.
+     *
+     * The first argument is a either an open stream or a path to use to open
+     * a text file. In either case, the input stream will automatically be
+     * closed when the reader object is destroyed. Any additional arguments are
+     * passed along to the reader's constructor.
+     */
+    public static function open(/* $args */)
+    {
+        // This is strictly for documention purposes. This should return a
+        // dynamic type, so derived classes must implement their own open()
+        // method if appropriate. Here is a sample implementation.
+        //
+        // if (!($args = func_get_args())) {
+        //     $message = "call to open() is missing required arguments";
+        //     throw new BadMethodCallException($message);
+        // }
+        // if (!is_resource($args[0])) {
+        //     // Assume this is a string to use as a file path.
+        //     $args[0] = fopen($path, 'r');
+        // }
+        // $class = new ReflectionClass('Derived_Class_Name_Goes_Here');
+        // $reader = $class->newInstanceArgs($args);
+        // $reader->closing = true;  // take responsiblity for closing stream
+        // return $reader;
+        $message = 'Serial_Core_TabularReader::open() is not implemented';
+        throw new BadMethodCallException($message);
+    }
     
     /**
      * Initialize this object.
@@ -49,5 +80,19 @@ abstract class Serial_Core_TabularReader extends Serial_Core_Reader
             $record[$field->name] = $field->decode($tokens[$pos++]);
         }
         return $record;
+    }
+    
+    /**
+     * Object clean-up.
+     *
+     * If the $closing attribute is true, this object's stream is automatically
+     * closed; see the open() method.
+     */
+    public function __destruct()
+    {
+        if ($this->closing) {
+            @fclose($this->stream);
+        }
+        return;
     }    
 }
