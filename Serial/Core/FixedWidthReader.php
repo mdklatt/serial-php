0162864c16763f8cb508600d9b2d2bea03b0108e
Michael Klatt <mdklatt@ou.edu>
2014-02-27 17:20:37 -0600

    added FixedWidthReader::open() method

--- Serial/Core/FixedWidthReader.php
+++ Serial/Core/FixedWidthReader.php
@@ -8,6 +8,33 @@
 class Serial_Core_FixedWidthReader extends Serial_Core_TabularReader
 {
     /**
+     * Open a FixedWidhtReader with automatic stream-handling.
+     *
+     * The first argument is a either an open stream or a path to use to open
+     * a text file. In either case, the input stream will automatically be
+     * closed when the reader object is destroyed. Any additional arguments are
+     * passed along to the FixedWidthReader constructor.
+     */
+    public static function open(/* $args */)
+    {
+        // Every derived class *MUST* implement its own open() method that
+        // returns the correct type of object.
+        $args = func_get_args();
+        if (count($args) < 2) {
+            $message = 'call to open() is missing required arguments';
+            throw new BadMethodCallException($message);
+        }
+        if (!is_resource($args[0])) {
+            // Assume this is a string to use as a file path.
+            $args[0] = fopen($path, 'r');
+        }
+        $class = new ReflectionClass('Serial_Core_FixedWidthReader');
+        $reader = $class->newInstanceArgs($args);
+        $reader->closing = true;  // take responsiblity for closing stream
+        return $reader;
+    }
+
+    /**
      * Split a line of text into an array of string tokens.
      *
      * Lines are split based on the string position of each field.
--- Test/FixedWidthReaderTest.php
+++ Test/FixedWidthReaderTest.php
@@ -13,17 +13,33 @@ class Test_FixedWidthReaderTest extends Test_TabularReaderTest
      */
     protected function setUp()
     {
-        $array_fields = array(
-            new Serial_Core_StringField('x', array(0, 3), '%3s'),
-            new Serial_Core_StringField('y', array(3, 3), '%3s'),
-        );
-        $fields = array(
+        $this->fields = array(
             new Serial_Core_IntField('int', array(0, 3), '%3d'),
-            new Serial_Core_ArrayField('arr', array(3, null), $array_fields), 
+            new Serial_Core_ArrayField('arr', array(3, null), array(
+                new Serial_Core_StringField('x', array(0, 3), '%3s'),
+                new Serial_Core_StringField('y', array(3, 3), '%3s'),                
+            )), 
         );
         $this->data = "123abcdef\n456ghijkl\n";
         parent::setUp();
-        $this->reader = new Serial_Core_FixedWidthReader($this->stream, $fields);        
+        $this->reader = new Serial_Core_FixedWidthReader($this->stream, 
+                        $this->fields);        
+        return;
+    }
+
+    /**
+     * Test the open() method.
+     * 
+     */
+    public function testOpen()
+    {
+        // TODO: This only tests an open stream; also need to test with a file
+        // path.
+        $this->reader = Serial_Core_FixedWidthReader::open($this->stream, 
+                        $this->fields);
+        $this->test_iter();
+        unset($this->reader);  // close $this->stream
+        $this->assertFalse(is_resource($this->stream));
         return;
     }
 }
