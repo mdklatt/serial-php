721365dbd2d4a4a61a8d810a046b1f6fb86d223c
Michael Klatt <mdklatt@ou.edu>
2014-09-19 10:59:20 -0500

    refactored SortReader to use SortQueue

---
+++ lib/Serial/Core/SortQueue.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Sort a sequence of records.
+ *
+ * This is used internally by SortReader and SortWriter and is not part of the
+ * public library API.
+ */
+class Serial_Core_SortQueue
+{
+    public $sorted = array();
+
+    private $keyFunc;
+    private $groupFunc;
+    private $groupKey;
+    private $buffer = array();
+    
+    public function __construct($key, $group=null)
+    {
+        if (!is_callable($key)) {
+            // Use the default key function.
+            $key = array(new Serial_Core_KeyFunc($key), '__invoke');
+        }
+        $this->keyFunc = $key;
+        if ($group && !is_callable($group)) {
+            // Use the default key function.
+            $group = array(new Serial_Core_KeyFunc($group), '__invoke');
+        }
+        $this->groupFunc = $group;
+        return;        
+    }
+    
+    public function push($record)
+    {
+        if ($this->groupFunc) {
+            $groupKey = call_user_func($this->groupFunc, $record);
+            if ($groupKey != $this->groupKey) {
+                // This is a new group, flush the previous group.
+                $this->flush();
+            }
+            $this->groupKey = $groupKey;
+        }
+        $this->buffer[] = $record;
+    }
+        
+    public function flush()
+    {
+        if (!$this->buffer) {
+            return;
+        }
+        $keyCols = array();
+        foreach ($this->buffer as $row => $record) {
+            // Build an N x K array of key values to use to with multisort.
+            $keyCols[] = call_user_func($this->keyFunc, $record); 
+        }
+        array_multisort($keyCols, $this->buffer);
+        $this->sorted = $this->buffer;
+        $this->buffer = array();
+        return;        
+    }
+}
--- lib/Serial/Core/SortReader.php
+++ lib/Serial/Core/SortReader.php
@@ -5,22 +5,15 @@
  */
 class Serial_Core_SortReader extends Serial_Core_ReaderBuffer
 {
-    // Except for the additional uflow() method this has essentially the same
-    // implementation as and should be kept in sync with SortWriter. Hooray for
-    // the lack of multiple inheritence!
-    // TODO: Factor common sorting code out as some kind of Sorter object?
+    private $buffer;
     
-    private $buffer = array();
-    private $keyfunc;
-    private $groupfunc;
-    private $groupval;
-        
     /**
      * Initialize this object.
      *
      * The $key argument is either a single field name, an array of names, or
      * a custom key function that returns the sort key values to use for each 
-     * record.
+     * record. PHP sorts are not stable; the order of records with the same
+     * key value is not preserved.
      *
      * The optional $group argument is like the $key argument but is used to
      * group records that are already partially sorted. Records will be sorted
@@ -31,16 +24,8 @@ class Serial_Core_SortReader extends Serial_Core_ReaderBuffer
     public function __construct($reader, $key, $group=null)
     {
         parent::__construct($reader);
-        if (!is_callable($key)) {
-            // Use the default key function.
-            $key = array(new Serial_Core_KeyFunc($key), '__invoke');
-        }
-        $this->keyfunc = $key;
-        if ($group && !is_callable($group)) {
-            // Use the default key function.
-            $group = array(new Serial_Core_KeyFunc($group), '__invoke');
-        }
-        $this->groupfunc = $group;
+        $this->buffer = new Serial_Core_SortQueue($key, $group);
+        $this->output = &$this->buffer->sorted;
         return;
     }
 
@@ -52,15 +37,7 @@ class Serial_Core_SortReader extends Serial_Core_ReaderBuffer
      */
     protected function queue($record)
     {
-        if ($this->groupfunc) {
-            $groupval = call_user_func($this->groupfunc, $record);
-            if ($groupval != $this->groupval) {
-                // This is a new group, process the previous group.
-                $this->flush();
-            }
-            $this->groupval = $groupval;
-        }
-        $this->buffer[] = $record;
+        $this->buffer->push($record);
         return;
     }
     
@@ -72,29 +49,10 @@ class Serial_Core_SortReader extends Serial_Core_ReaderBuffer
      */
     protected function uflow()
     {
-        if (!$this->buffer) {
+        $this->buffer->flush();
+        if (!$this->output) {
             throw new Serial_Core_StopIteration();
         }
-        $this->flush();
-        return;
-    }
-    
-    /**
-     * Send sorted records to the output queue.
-     */
-    private function flush()
-    {
-        if (!$this->buffer) {
-            return;
-        }
-        $keycols = array();
-        foreach ($this->buffer as $row => $record) {
-            // Build an N x K array of key values to use to with multisort.
-            $keycols[] = call_user_func($this->keyfunc, $record); 
-        }
-        array_multisort($keycols, $this->buffer);
-        $this->output = $this->buffer;
-        $this->buffer = array();
         return;
     }
 }
