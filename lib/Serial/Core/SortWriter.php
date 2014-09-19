d12f1ceaa1debdbd6fc74e5d350c6c8fc8f1b7ce
Michael Klatt <mdklatt@ou.edu>
2014-09-19 11:07:17 -0500

    refactored SortWriter to use SortQueue

--- lib/Serial/Core/SortWriter.php
+++ lib/Serial/Core/SortWriter.php
@@ -5,16 +5,8 @@
  */
 class Serial_Core_SortWriter extends Serial_Core_WriterBuffer
 {
-    // Except for the lack of a uflow() method this has essentially the same
-    // implementation as and should be kept in sync with SortReader. Hooray for
-    // the lack of multiple inheritence!
-    // TODO: Factor common sorting code out as some kind of Sorter object?
+    private $buffer;
 
-    private $buffer = array();
-    private $keyfunc;
-    private $groupfunc;
-    private $groupval;
-    
     /**
      * Initialize this object.
      *
@@ -31,55 +23,29 @@ class Serial_Core_SortWriter extends Serial_Core_WriterBuffer
     public function __construct($writer, $key, $group=null)
     {
         parent::__construct($writer);
-        if (!is_callable($key)) {
-            // Use the default key function.
-            $key = array(new Serial_Core_KeyFunc($key), '__invoke');
-        }
-        $this->keyfunc = $key;
-        if ($group && !is_callable($group)) {
-            // Use the default key function.
-            $group = array(new Serial_Core_KeyFunc($group), '__invoke');
-        }
-        $this->groupfunc = $group;
+        $this->buffer = new Serial_Core_SortQueue($key, $group);
+        $this->output = &$this->buffer->sorted;
         return;
     }
 
     /**
      * Process an incoming record.
      *
-     * Records are buffered until the the input reader has been consumed or
-     * the beginning of a new group is encountered.
+     * Records are buffered until the beginning of a new group is encountered
+     * or flush() is called.
      */
     protected function queue($record)
     {
-        if ($this->groupfunc) {
-            $groupval = call_user_func($this->groupfunc, $record);
-            if ($groupval != $this->groupval) {
-                // This is a new group, process the previous group.
-                $this->flush();
-            }
-            $this->groupval = $groupval;
-        }
-        $this->buffer[] = $record;
+        $this->buffer->push($record);
         return;
     }
     
     /**
-     * Send sorted records to the output queue.
+     * Send all buffered records to the output queue.
      */
     protected function flush()
     {
-        if (!$this->buffer) {
-            return;
-        }
-        $keycols = array();
-        foreach ($this->buffer as $row => $record) {
-            // Build an N x K array of key values to use to with multisort.
-            $keycols[] = call_user_func($this->keyfunc, $record); 
-        }
-        array_multisort($keycols, $this->buffer);
-        $this->output = $this->buffer;
-        $this->buffer = array();
+        $this->buffer->flush();
         return;
     }
 }
