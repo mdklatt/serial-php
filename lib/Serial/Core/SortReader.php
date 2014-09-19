6c5d8b346e9086909a8176417002aed786aaa1c0
Michael Klatt <mdklatt@ou.edu>
2014-09-18 22:47:17 -0500

    expanded comments [ci skip]

--- lib/Serial/Core/SortReader.php
+++ lib/Serial/Core/SortReader.php
@@ -14,12 +14,19 @@ class Serial_Core_SortReader extends Serial_Core_ReaderBuffer
     private $keyfunc;
     private $groupfunc;
     private $groupval;
-    
-    
+        
     /**
      * Initialize this object.
      *
-     * The $key argument is either a single field name or an array of names.
+     * The $key argument is either a single field name, an array of names, or
+     * a custom key function that returns the sort key values to use for each 
+     * record.
+     *
+     * The optional $group argument is like the $key argument but is used to
+     * group records that are already partially sorted. Records will be sorted
+     * within each group rather than as a single sequence. If the groups are
+     * small relative to the total sequence length this can significantly 
+     * improve performance and memory usage.
      */
     public function __construct($reader, $key, $group=null)
     {
@@ -39,6 +46,9 @@ class Serial_Core_SortReader extends Serial_Core_ReaderBuffer
 
     /**
      * Process an incoming record.
+     *
+     * Records are buffered until the the input reader has been consumed or
+     * the beginning of a new group is encountered.
      */
     protected function queue($record)
     {
--- lib/Serial/Core/SortWriter.php
+++ lib/Serial/Core/SortWriter.php
@@ -18,7 +18,15 @@ class Serial_Core_SortWriter extends Serial_Core_WriterBuffer
     /**
      * Initialize this object.
      *
-     * The $key argument is either a single field name or an array of names.
+     * The $key argument is either a single field name, an array of names, or
+     * a custom key function that returns the sort key values to use for each 
+     * record.
+     *
+     * The optional $group argument is like the $key argument but is used to
+     * group records that are already partially sorted. Records will be sorted
+     * within each group rather than as a single sequence. If the groups are
+     * small relative to the total sequence length this can significantly 
+     * improve performance and memory usage.
      */
     public function __construct($writer, $key, $group=null)
     {
@@ -38,6 +46,9 @@ class Serial_Core_SortWriter extends Serial_Core_WriterBuffer
 
     /**
      * Process an incoming record.
+     *
+     * Records are buffered until the the input reader has been consumed or
+     * the beginning of a new group is encountered.
      */
     protected function queue($record)
     {
