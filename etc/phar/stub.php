a093f4dd4d10b3354caf99d9d19887d8f813051a
Michael Klatt <mdklatt@ou.edu>
2015-11-18 22:11:24 -0600

    refactored Phing tasks

--- etc/phar/stub.php
+++ etc/phar/stub.php
@@ -7,6 +7,6 @@
  * during the Phar creation process.
  */
 Phar::mapPhar();
-include 'phar://'.__FILE__.DIRECTORY_SEPARATOR.'Serial/Core/autoload.php';
+require 'phar://'.__FILE__.DIRECTORY_SEPARATOR.'Serial/Core/autoload.php';
 __HALT_COMPILER(); 
 ?>
